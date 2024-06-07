48fac94f07e5ae1bebe3ba039d40edaf9422a81d
y123an <yohannesaregahabte@gmail.com>
2024-06-07 15:10:11 +0300

    updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { registerLicense } from "@syncfusion/ej2-base";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NBaF1cXmhPYVJyWmFZfVpgfF9GYFZUQGYuP1ZhSXxXdkNjXn9WcHRQQmdeVUE="
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
